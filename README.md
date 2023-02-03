b94d8f29c5e6186b0cf717490332eff08bbd58eb
Alan Caruso <alan.carusoo@gmail.com>
2023-02-03 16:27:50 -0300

    init commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # front-web-react
  front view for Aura Dev admin platform
+ 
+# install packages
+npm install
+# run dev
+npm run dev
---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import Home from "./Pages/Home/Home"
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <h1>Welcome to admin blog</h1>
+      <Home />
+    </div>
+  )
+}
+
+export default App
