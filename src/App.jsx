947c0daf6dd0ee6e3e0a9d7e5dc355a3f6f157ef
Alan Caruso <alan.carusoo@gmail.com>
2023-02-04 16:47:47 -0300

    card component update

--- src/App.jsx
+++ src/App.jsx
@@ -15,23 +15,31 @@ function App() {
     <div className="App">
       <h1>Welcome to admin blog</h1>
       <Home />
-      {post.map(i => (
-        <div key={i.titulo} className="max-w-2xl bg-white border-2 border-gray-300 p-5 rounded-md tracking-wide shadow-lg">
-          <div id="header" className="flex">
-            <img alt="mountain" className="w-45 rounded-md border-2 border-gray-300 w-300" src={i.img} />
-            <div id="body" className="flex flex-col ml-5">
-              <h4 id="name" className="text-xl font-semibold mb-2">{i.titulo}</h4>
-              <p id="job" className="text-gray-800 mt-2">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-              <div className="flex mt-5">
-                <img alt="avatar" className="w-6 rounded-full border-2 border-gray-300" src="https://picsum.photos/seed/picsum/200" />
-                <p className="ml-3">John Doe</p>
-              </div>
+
+
+      <section className="text-gray-600 body-font">
+        <div className="container px-5 py-24 mx-auto max-w-7x1 flex flex-wrap -m-4">
+          <div className="flex flex-wrap w-full mb-4 p-4">
+            <div className="w-full mb-6 lg:mb-0">
+              <h1 className="sm:text-4xl text-5xl font-medium font-bold title-font mb-2 text-gray-900">News</h1>
+              <div className="h-1 w-20 bg-indigo-500 rounded"></div>
             </div>
           </div>
+          {
+            post.map(i => (
+              <div key={i.titulo} className="xl:w-1/3 md:w-1/2 p-4">
+                <div className="bg-white p-6 rounded-lg">
+                  <img className="lg:h-60 xl:h-56 md:h-64 sm:h-72 xs:h-72 h-72 rounded w-full object-cover object-center mb-6" src={i.img} alt="Image Size 720x400" />
+                  <h3 className="tracking-widest text-indigo-500 text-xs font-medium title-font">{i.creador.nombre}</h3>
+                  <h2 className="text-lg text-gray-900 font-medium title-font mb-4">{i.titulo}</h2>
+                  <p className="leading-relaxed text-base">{i.descripcion}</p>
+                </div>
+              </div>
+            ))
+          }
+
         </div>
-      ))}
-
-
+      </section >
     </div>
   )
 }
