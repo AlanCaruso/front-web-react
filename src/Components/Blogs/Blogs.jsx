47173faa9b7f69036ba5ffad302f78ce786a4df3
Alan Caruso <alan.carusoo@gmail.com>
2023-06-08 10:29:57 -0300

    update data

--- src/Components/Blogs/Blogs.jsx
+++ src/Components/Blogs/Blogs.jsx
@@ -1,36 +1,34 @@
-import React from 'react'
-import Item from "../Item/Item"
+import React from "react";
+import Item from "../Item/Item";
 
 function Blogs({ posts }) {
-    return (
-        <section className="text-gray-600 body-font">
-            <div className="container px-5 py-24 mx-auto max-w-7x1 flex flex-wrap -m-4">
-                <div className="flex flex-wrap w-full mb-4 p-4">
-                    <div className="w-full mb-6 lg:mb-0">
-                        <h1 className="sm:text-4xl text-5xl font-medium font-bold title-font mb-2 text-gray-900">News</h1>
-                        <div className="h-1 w-20 bg-indigo-500 rounded"></div>
-                    </div>
-                </div>
-                {
-                    posts.map(i => (
-                        <Item
-                            id={i._id}
-                            img={i.img}
-                            email={i.creador.email}
-                            title={i.titulo}
-                            description={i.descripcion}
-                            creatorImg={i.creador.img}
-                            creatorName={i.creador.nombre}
-                            itemKey={i._id}
-                            key={i._id}
-
-                        />
-                    ))
-                }
-
-            </div>
-        </section >
-    )
+  return (
+    <section className="text-gray-600 body-font">
+      <div className="container px-5 py-24 mx-auto max-w-7x1 flex flex-wrap -m-4">
+        <div className="flex flex-wrap w-full mb-4 p-4">
+          <div className="w-full mb-6 lg:mb-0">
+            <h1 className="sm:text-4xl text-5xl font-medium font-bold title-font mb-2 text-gray-900">
+              Publicaciones
+            </h1>
+            <div className="h-1 w-20 bg-indigo-500 rounded"></div>
+          </div>
+        </div>
+        {posts.map((i) => (
+          <Item
+            id={i._id}
+            img={i.img}
+            email={i.creador.email}
+            title={i.titulo}
+            description={i.descripcion}
+            creatorImg={i.creador.img}
+            creatorName={i.creador.nombre}
+            itemKey={i._id}
+            key={i._id}
+          />
+        ))}
+      </div>
+    </section>
+  );
 }
 
-export default Blogs
+export default Blogs;
