42cd7937595aaee20ef28a529fed181a72759190
Alan Caruso <alan.carusoo@gmail.com>
2023-06-07 23:34:58 -0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,17 @@
 import Blogs from "./Components/Blogs/Blogs"
 import BlogPage from "./Components/BlogPage/BlogPage"
-import { getPosts } from "./Posts"
+import { items } from "./data"
 import { Routes, Route } from 'react-router-dom';
 import './App.css'
-import { useState, useEffect } from 'react'
 
 function App() {
-  const [posts, setPosts] = useState([])
-  useEffect(() => {
-    getPosts().then((data) => setPosts(data.data))
-
-  }, [])
 
 
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Blogs posts={posts} />}></Route>
-        <Route path="blogs/:blogId" element={<BlogPage posts={posts} />} />
+        <Route path="/" element={<Blogs posts={items.data} />}></Route>
+        <Route path="blogs/:blogId" element={<BlogPage posts={items.data} />} />
       </Routes>
     </div>
   )
--- src/Posts.jsx
+++
@@ -1,7 +0,0 @@
-export async function getPosts() {
-    const response = await fetch("http://localhost:3030/blog")
-
-    const data = await response.json()
-
-    return data;
-}
