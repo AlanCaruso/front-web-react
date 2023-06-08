a8e9435d31870f6d2e898b12195499cf655a3636
Alan Caruso <alan.carusoo@gmail.com>
2023-06-07 23:44:00 -0300

    update

--- src/data.js
+++ src/data.js
@@ -1,122 +1,114 @@
 export const items = {
-    "total": 3,
-    "limit": 10,
-    "skip": 0,
-    "data": [
-        {
-            "_id": "63dc74f19a297e5bff568ea9",
-            "titulo": "El Misterio de Charly G 2",
-            "descripcion": "El hombre que logro llegar a la luna en menos de 10 minutos.",
-            "img": "https://res.cloudinary.com/doznjtpmk/image/upload/v1675399361/admin-web/ji6ibpoxh8azesqdcuqt.jpg",
-            "creador": {
-                "_id": "63dc9ff393f1795edfce4a18",
-                "email": "carlo.gammarota@gmail.com",
-                "img": "https://res.cloudinary.com/doznjtpmk/image/upload/v1675403841/admin-web/u11cl8wmyylshiawsurb.jpg",
-                "nombre": "Carlo Gammarota",
-                "role": "customer",
-                "descripcion": "Me dedico al desarrollo web mi de la aplicacion completa",
-                "permissions": [
-                    "admin"
-                ],
-                "createdAt": "2023-02-03T05:47:31.241Z",
-                "updatedAt": "2023-02-03T06:21:55.703Z",
-                "__v": 0
-            },
-            "createdAt": "2023-02-03T04:43:37.535Z",
-            "updatedAt": "2023-02-03T06:22:05.118Z"
-        },
-        {
-            "_id": "63dc901c7e99ad90f263037b",
-            "titulo": "Hoy me desperte contento 2",
-            "descripcion": "Gracias al grandioso día hoy no fue cualquier día, hoy pude despertarme feliz y lograr mis metas.",
-            "img": "https://res.cloudinary.com/doznjtpmk/image/upload/v1675399193/admin-web/vobmqrbawneivl6yfxys.jpg",
-            "creador": {
-                "_id": "63dc9ff393f1795edfce4a18",
-                "email": "carlo.gammarota@gmail.com",
-                "img": "https://res.cloudinary.com/doznjtpmk/image/upload/v1675403841/admin-web/u11cl8wmyylshiawsurb.jpg",
-                "nombre": "Carlo Gammarota",
-                "role": "customer",
-                "descripcion": "Me dedico al desarrollo web mi de la aplicacion completa",
-                "permissions": [
-                    "admin"
-                ],
-                "createdAt": "2023-02-03T05:47:31.241Z",
-                "updatedAt": "2023-02-03T06:21:55.703Z",
-                "__v": 0
-            },
-            "createdAt": "2023-02-03T04:39:56.163Z",
-            "updatedAt": "2023-02-03T06:22:14.275Z",
-            "__v": 0
-        },
-        {
-            "_id": "63dd4708868cd270443c42c1",
-            "titulo": "Curso Javascript Esencial",
-            "descripcion": "Todo lo fundamental que deberias saber sobre javascript",
-            "img": "https://res.cloudinary.com/doznjtpmk/image/upload/v1675446149/admin-web/wbqumyv7bktzudoeo9gv.webp",
-            "creador": {
-                "_id": "63dd45b68c2b5720f7c0227d",
-                "email": "alan.carusoo@gmail.com",
-                "img": "https://res.cloudinary.com/doznjtpmk/image/upload/v1675445991/admin-web/uiavxxd5ofki0shys2j3.png",
-                "nombre": "Alan Caruso",
-                "role": "customer",
-                "descripcion": "Hago el front - end",
-                "permissions": [
-                    "admin"
-                ],
-                "createdAt": "2023-02-03T17:34:46.477Z",
-                "updatedAt": "2023-02-03T17:40:29.067Z",
-                "__v": 0
-            },
-            "createdAt": "2023-02-03T17:42:32.602Z",
-            "updatedAt": "2023-02-03T17:42:32.602Z",
-            "__v": 0
-        },
-        {
-            "_id": "63dd4748869cd273443c42c1",
-            "titulo": "Curso React js Introduccion",
-            "descripcion": "Curso introductorio a React js con Vite como herramienta para crear un proyecto rapido y completo.",
-            "img": "/src/assets/reactjs.jpg",
-            "creador": {
-                "_id": "63dd45b68c2b5720f7c0227d",
-                "email": "alan.carusoo@gmail.com",
-                "img": "https://res.cloudinary.com/doznjtpmk/image/upload/v1675445991/admin-web/uiavxxd5ofki0shys2j3.png",
-                "nombre": "Alan Caruso",
-                "role": "customer",
-                "descripcion": "Hago el front - end",
-                "permissions": [
-                    "admin"
-                ],
-                "createdAt": "2023-02-03T17:34:46.477Z",
-                "updatedAt": "2023-02-03T17:40:29.067Z",
-                "__v": 0
-            },
-            "createdAt": "2023-02-03T17:42:32.602Z",
-            "updatedAt": "2023-02-03T17:42:32.602Z",
-            "__v": 0
-        },
-        {
-            "_id": "63dd4743868cd273443c42c1",
-            "titulo": "Que es ChatGPT",
-            "descripcion": "Cómo funciona ChatGPT: el chatbot más avanzado del mundo",
-            "img": "/src/assets/chatgpt.png",
-            "creador": {
-                "_id": "63dd45b68c2b5720f7c0227d",
-                "email": "alan.carusoo@gmail.com",
-                "img": "https://res.cloudinary.com/doznjtpmk/image/upload/v1675445991/admin-web/uiavxxd5ofki0shys2j3.png",
-                "nombre": "Alan Caruso",
-                "role": "customer",
-                "descripcion": "Hago el front - end",
-                "permissions": [
-                    "admin"
-                ],
-                "createdAt": "2023-02-03T17:34:46.477Z",
-                "updatedAt": "2023-02-03T17:40:29.067Z",
-                "__v": 0
-            },
-            "createdAt": "2023-02-03T17:42:32.602Z",
-            "updatedAt": "2023-02-03T17:42:32.602Z",
-            "__v": 0
-        },
-
-    ]
-}
+  total: 3,
+  limit: 10,
+  skip: 0,
+  data: [
+    {
+      _id: "63dc74f19a297e5bff568ea9",
+      titulo: "El Misterio de Charly G 2",
+      descripcion:
+        "El hombre que logro llegar a la luna en menos de 10 minutos.",
+      img: "https://res.cloudinary.com/doznjtpmk/image/upload/v1675399361/admin-web/ji6ibpoxh8azesqdcuqt.jpg",
+      creador: {
+        _id: "63dc9ff393f1795edfce4a18",
+        email: "carlo.gammarota@gmail.com",
+        img: "https://res.cloudinary.com/doznjtpmk/image/upload/v1675403841/admin-web/u11cl8wmyylshiawsurb.jpg",
+        nombre: "Carlo Gammarota",
+        role: "customer",
+        descripcion: "Me dedico al desarrollo web mi de la aplicacion completa",
+        permissions: ["admin"],
+        createdAt: "2023-02-03T05:47:31.241Z",
+        updatedAt: "2023-02-03T06:21:55.703Z",
+        __v: 0,
+      },
+      createdAt: "2023-02-03T04:43:37.535Z",
+      updatedAt: "2023-02-03T06:22:05.118Z",
+    },
+    {
+      _id: "63dc901c7e99ad90f263037b",
+      titulo: "Hoy me desperte contento 2",
+      descripcion:
+        "Gracias al grandioso día hoy no fue cualquier día, hoy pude despertarme feliz y lograr mis metas.",
+      img: "https://res.cloudinary.com/doznjtpmk/image/upload/v1675399193/admin-web/vobmqrbawneivl6yfxys.jpg",
+      creador: {
+        _id: "63dc9ff393f1795edfce4a18",
+        email: "carlo.gammarota@gmail.com",
+        img: "https://res.cloudinary.com/doznjtpmk/image/upload/v1675403841/admin-web/u11cl8wmyylshiawsurb.jpg",
+        nombre: "Carlo Gammarota",
+        role: "customer",
+        descripcion: "Me dedico al desarrollo web mi de la aplicacion completa",
+        permissions: ["admin"],
+        createdAt: "2023-02-03T05:47:31.241Z",
+        updatedAt: "2023-02-03T06:21:55.703Z",
+        __v: 0,
+      },
+      createdAt: "2023-02-03T04:39:56.163Z",
+      updatedAt: "2023-02-03T06:22:14.275Z",
+      __v: 0,
+    },
+    {
+      _id: "63dd4708868cd270443c42c1",
+      titulo: "Curso Javascript Esencial",
+      descripcion: "Todo lo fundamental que deberias saber sobre javascript",
+      img: "https://res.cloudinary.com/doznjtpmk/image/upload/v1675446149/admin-web/wbqumyv7bktzudoeo9gv.webp",
+      creador: {
+        _id: "63dd45b68c2b5720f7c0227d",
+        email: "alan.carusoo@gmail.com",
+        img: "https://res.cloudinary.com/doznjtpmk/image/upload/v1675445991/admin-web/uiavxxd5ofki0shys2j3.png",
+        nombre: "Alan Caruso",
+        role: "customer",
+        descripcion: "Hago el front - end",
+        permissions: ["admin"],
+        createdAt: "2023-02-03T17:34:46.477Z",
+        updatedAt: "2023-02-03T17:40:29.067Z",
+        __v: 0,
+      },
+      createdAt: "2023-02-03T17:42:32.602Z",
+      updatedAt: "2023-02-03T17:42:32.602Z",
+      __v: 0,
+    },
+    {
+      _id: "63dd4748869cd273443c42c1",
+      titulo: "Curso React js Introduccion",
+      descripcion:
+        "Curso introductorio a React js con Vite como herramienta para crear un proyecto rapido y completo.",
+      img: "https://res.cloudinary.com/dqv8tp0j3/image/upload/v1686192173/reactjs_mild0k.jpg",
+      creador: {
+        _id: "63dd45b68c2b5720f7c0227d",
+        email: "alan.carusoo@gmail.com",
+        img: "https://res.cloudinary.com/doznjtpmk/image/upload/v1675445991/admin-web/uiavxxd5ofki0shys2j3.png",
+        nombre: "Alan Caruso",
+        role: "customer",
+        descripcion: "Hago el front - end",
+        permissions: ["admin"],
+        createdAt: "2023-02-03T17:34:46.477Z",
+        updatedAt: "2023-02-03T17:40:29.067Z",
+        __v: 0,
+      },
+      createdAt: "2023-02-03T17:42:32.602Z",
+      updatedAt: "2023-02-03T17:42:32.602Z",
+      __v: 0,
+    },
+    {
+      _id: "63dd4743868cd273443c42c1",
+      titulo: "Que es ChatGPT",
+      descripcion: "Cómo funciona ChatGPT: el chatbot más avanzado del mundo",
+      img: "https://res.cloudinary.com/dqv8tp0j3/image/upload/v1686192175/chatgpt_s8bszt.png",
+      creador: {
+        _id: "63dd45b68c2b5720f7c0227d",
+        email: "alan.carusoo@gmail.com",
+        img: "https://res.cloudinary.com/doznjtpmk/image/upload/v1675445991/admin-web/uiavxxd5ofki0shys2j3.png",
+        nombre: "Alan Caruso",
+        role: "customer",
+        descripcion: "Hago el front - end",
+        permissions: ["admin"],
+        createdAt: "2023-02-03T17:34:46.477Z",
+        updatedAt: "2023-02-03T17:40:29.067Z",
+        __v: 0,
+      },
+      createdAt: "2023-02-03T17:42:32.602Z",
+      updatedAt: "2023-02-03T17:42:32.602Z",
+      __v: 0,
+    },
+  ],
+};
